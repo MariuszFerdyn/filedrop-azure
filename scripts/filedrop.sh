60c55771add08a08c5b57d1a7a8e2abfb68090b2
Mariusz Ferdyn <MariuszFerdyn@users.noreply.github.com>
2021-02-03 21:27:38 +0100

    Update filedrop.sh

--- scripts/filedrop.sh
+++ scripts/filedrop.sh
@@ -2,5 +2,5 @@
 apt-get update
 cd /tmp
 wget https://raw.githubusercontent.com/MariuszFerdyn/filedrop-azure/main/scripts/filedrop2.sh
-chmod 755 filedrop.sh
-./filedrop.sh
+chmod 755 filedrop2.sh
+./filedrop2.sh
