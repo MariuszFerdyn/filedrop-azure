08522067358f45272b1f9dc5123f13cc223082f7
Mariusz Ferdyn <MariuszFerdyn@users.noreply.github.com>
2021-02-05 15:57:56 +0100

    Update filedrop.sh

--- scripts/filedrop.sh
+++ scripts/filedrop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 apt-get update
-cd /tmp
-wget https://raw.githubusercontent.com/MariuszFerdyn/filedrop-azure/main/scripts/filedrop2.sh
-chmod 755 filedrop2.sh
-./filedrop2.sh
+useradd -m glasswall
+#userdel -r myinstalluser
+#cd /tmp
+wget https://raw.githubusercontent.com/MariuszFerdyn/filedrop-azure/main/scripts/filedrop2.sh -O ~glasswall/filedrop2.sh
+chmod 755 ~glasswall/filedrop2.sh
+su -l glasswall ./filedrop2.sh
