fa5064d0dc7aa546ef7c3e6c39a797b526b5215f
Mariusz Ferdyn <MariuszFerdyn@users.noreply.github.com>
2021-02-09 20:19:50 +0100

    Update filedrop.sh

--- scripts/filedrop.sh
+++ scripts/filedrop.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 apt-get update
 useradd -m glasswall
-#userdel -r myinstalluser
-#cd /tmp
-wget https://raw.githubusercontent.com/MariuszFerdyn/filedrop-azure/main/scripts/filedrop2.sh -O ~glasswall/filedrop2.sh
+wget https://raw.githubusercontent.com/MariuszFerdyn/filedrop-azure/main/scripts/filedrop2.sh
+-O ~glasswall/filedrop2.sh
 chmod 755 ~glasswall/filedrop2.sh
+echo 'glasswall ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/glasswall
+echo 0440 /etc/sudoers.d/glasswall
 su -l glasswall ./filedrop2.sh
+rm -f /etc/sudoers.d/glasswall
+userdel -r glasswall
